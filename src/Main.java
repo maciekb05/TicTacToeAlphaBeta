f208e72fccee940bbf544485e2d96f867d027867
Maciej Bielech <maciej.bielech@gmail.com>
2018-07-13 01:33:28 +0200

    commit from Windows

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Game game = new Game();
         game.play();
+        System.out.printf("Koniec");
     }
 }
